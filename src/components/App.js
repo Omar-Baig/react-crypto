91e8d2e135867e50e06ecb4f563457123c1c0d9d
Omar Baig <obaig992@gmail.com>
2021-01-23 16:21:46 -0500

    changes

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from "react-dom";
+
+class App extends React.Component{
+    render (){
+        return (
+            <div className="container text-center">React Crypto</div>
+        )
+    }
+}
+
+export default App;

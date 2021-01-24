135972614acd944c84ca2efe73d3ea7721463bd9
Omar Baig <obaig992@gmail.com>
2021-01-24 18:46:42 -0500

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -10,55 +10,37 @@ class App extends React.Component{
 
     // Create a async await function
     async componentDidMount(){
-        const response = await Coin.get("/tickers/?start=0&limit=100");
+        const response = await Coin.get("/tickers/?start=0&limit=99");
         var cleanData = response.data.data
         this.setState({currency: cleanData})
         console.log(cleanData)
     }
     
+    colorChange(){
+
+    }
+
     // render App here
     render (){
         return (
             <div className="container">
                 <h1 className="text-center">React Crypto Testing</h1>
-                <table className="table table-striped table-hover .tabl-sm">
-                    <thead>
-                    <tr>
-                        <th scope="col">Rank</th>
-                        <th scope="col">Symbol</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Change 1H</th>
-                        <th scope="col">Change 24H</th>
-                        <th scope="col">Change 7D</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {/*Creating a row for each crypto currency*/}
-                        {this.state.currency.map(el => (
-                            <tr key={el.id}>
-                                <th scope="row">{el.rank}</th> 
-                                <td>{el.symbol}</td>
-                                <td>{el.name}</td>
-                                <td>$ {el.price_usd}</td>
-                                <td>$ {el.percent_change_1h}</td>
-                                <td>$ {el.percent_change_24h}</td>
-                                <td>$ {el.percent_change_7d}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-
-                {/* <div>
+                {/*Creating a card for each crypto currency*/}
+                <div className="card-wrap">
                     {this.state.currency.map(el => (
-                        <div>
-                            {el.rank}
+                        <div key={el.id} className="card">
+                            <div className="rank">{el.rank}. </div>
+                            <div className="symbol">{el.symbol}</div>
+                            <div className="name">{el.name}</div>
+                            <div className="price price-current">${el.price_usd}</div>
+                            <div className="price price-1h"><span>1H: $</span>{el.percent_change_1h}</div>
+                            <div className="price price-24h"><span>24H: $</span>{el.percent_change_24h}</div>
+                            <div className="price price-7d"><span>7D: $</span>{el.percent_change_7d}</div>
                         </div>
                     ))}
-                </div> */}
+                </div>
             </div>
-        )
-    }
+        )}
 }
 
 export default App;

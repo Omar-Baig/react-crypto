8e1e413904c0038d75c45c6997d783e9c37a848f
Omar Baig <obaig992@gmail.com>
2021-01-24 17:37:25 -0500

    Successfully made api call and showed it on DOM.

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,61 @@
 import React from 'react';
-import Card from "./Card";
 import Coin from "./Coin";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends React.Component{
 
+    // state
     state = {currency: []}
 
+    // Create a async await function
     async componentDidMount(){
         const response = await Coin.get("/tickers/?start=0&limit=100");
-        console.log(response.data.data)
+        var cleanData = response.data.data
+        this.setState({currency: cleanData})
+        console.log(cleanData)
     }
-
-
+    
+    // render App here
     render (){
         return (
             <div className="container">
                 <h1 className="text-center">React Crypto Testing</h1>
-                <Card />
+                <table className="table table-striped table-hover .tabl-sm">
+                    <thead>
+                    <tr>
+                        <th scope="col">Rank</th>
+                        <th scope="col">Symbol</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Price</th>
+                        <th scope="col">Change 1H</th>
+                        <th scope="col">Change 24H</th>
+                        <th scope="col">Change 7D</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/*Creating a row for each crypto currency*/}
+                        {this.state.currency.map(el => (
+                            <tr key={el.id}>
+                                <th scope="row">{el.rank}</th> 
+                                <td>{el.symbol}</td>
+                                <td>{el.name}</td>
+                                <td>$ {el.price_usd}</td>
+                                <td>$ {el.percent_change_1h}</td>
+                                <td>$ {el.percent_change_24h}</td>
+                                <td>$ {el.percent_change_7d}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                {/* <div>
+                    {this.state.currency.map(el => (
+                        <div>
+                            {el.rank}
+                        </div>
+                    ))}
+                </div> */}
             </div>
         )
     }

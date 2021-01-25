f8fd00f13978a8651c0883cad4304e712c8984e3
Omar Baig <obaig992@gmail.com>
2021-01-25 17:55:08 -0500

    Added color changing based on number value.

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,9 @@ class App extends React.Component{
     }
     
     colorChange(){
-
+        if(this.setState.currency.percent_change_1h < 0){
+            console.log("negative")
+        }
     }
 
     // render App here
@@ -33,9 +35,9 @@ class App extends React.Component{
                             <div className="symbol">{el.symbol}</div>
                             <div className="name">{el.name}</div>
                             <div className="price price-current">${el.price_usd}</div>
-                            <div className="price price-1h"><span>1H: $</span>{el.percent_change_1h}</div>
-                            <div className="price price-24h"><span>24H: $</span>{el.percent_change_24h}</div>
-                            <div className="price price-7d"><span>7D: $</span>{el.percent_change_7d}</div>
+                            <div className="price price-1h" style={{color: Math.sign(el.percent_change_1h) === -1 ? "red" : "green"}}><span>1H: $</span>{el.percent_change_1h}</div>
+                            <div className="price price-24h" style={{color: Math.sign(el.percent_change_1h) === -1 ? "red" : "green"}}><span>24H: $</span>{el.percent_change_24h}</div>
+                            <div className="price price-7d" style={{color: Math.sign(el.percent_change_1h) === -1 ? "red" : "green"}}><span>7D: $</span>{el.percent_change_7d}</div>
                         </div>
                     ))}
                 </div>
